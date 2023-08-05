355809ae80c8302384ae999e870accc7139baa6f
kyrlian <63187993+kyrlian@users.noreply.github.com>
2023-08-05 19:53:24 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,29 +1,32 @@
-(function script() {
   'use strict';
-  var w, h;
+  ;
 
+  // config
+  let appversion='ap37-kyr';
+  let hideapps=["Google", "Freebox","Steam Chat","Steam Link"];
+  let appnameminwidth=8;
+  let notifstart=2;
+  let appstart =6;
+  let displayablenotifs =appstart-notifstart-1;//TODO: use it below
+  //
+  var w, h
   function init() {
-    ap37.setTextSize(11);
+    ap37.setTextSize(13);
 
     w = ap37.getScreenWidth();
     h = ap37.getScreenHeight();
 
     background.init();
-    print(0, 0, 'ap37-2a9f58c3');
+    print(3, 0, appversion);
     time.init();
     battery.init();
     notifications.init();
     apps.init();
-    markets.init();
-    transmissions.init();
-    print(w - 3, h - 1, 'EOF');
+    print(w - 5, h - 1, 'EOF');
 
     ap37.setOnTouchListener(function (x, y) {
       notifications.onTouch(x, y);
       apps.onTouch(x, y);
-      transmissions.onTouch(x, y);
-      lineGlitch.onTouch(x, y);
-      wordGlitch.onTouch(x, y);
     });
   }
 
@@ -31,7 +34,6 @@
 
   var background = {
     buffer: [],
-    bufferColors: [],
     pattern: '',
     printPattern: function (x0, xf, y) {
       print(x0, y,
@@ -39,13 +41,13 @@
         '#333333');
     },
     init: function () {
-      background.pattern = rightPad(script, h * w, ' ');
-
+      const chars='-._ /';//random chars for background
+      for (let i = 0; i < w*h; i++) {
+        background.pattern += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
       for (var i = 0; i < h; i++) {
         background.buffer.push(background.pattern.substr(i * w, w));
-        background.bufferColors.push(arrayFill('#333333', w));
       }
-
       ap37.printLines(background.buffer, '#333333');
     }
   };
@@ -83,19 +85,19 @@
       if (notifications.active) {
         var nots = ap37.getNotifications();
         notifications.list = nots;
-        for (var i = 0; i < 3; i++) {
-          var y = i + 2;
-          background.printPattern(0, w, y);
+        for (var i = 0; i < 3; i++) {// display max 3 notifications
+          var y = i + notifstart;// print notifications from line 2
+          background.printPattern(0, w, y);//erase line first
           if (i < nots.length) {
             nots[i].y = y;
-            if (i == 2 && nots.length > 3) {
-              nots[i].ellipsis = true;
+            if (i ==  2 && nots.length > 3) {
+              nots[i].ellipsis = true;// if last displayable notif and has more
             }
             notifications.printNotification(nots[i], false);
           }
         }
       } else {
-        print(0, 3, 'Activate notifications');
+        print(0,notifstart , 'Activate notifications');
       }
     },
     printNotification: function (notification, highlight) {
@@ -103,10 +105,10 @@
       if (notification.ellipsis) {
         var length = Math.min(name.length, w - 10);
         name = name.substring(0, length) + "... +" +
-          (notifications.list.length - 3);
+          (notifications.list.length - 3);//override last notification with: name... number of remaining notifs
       }
-      print(0, notification.y, name, highlight ? '#ff3333' : '#ffffff');
-      if (highlight) {
+      print(0, notification.y, name, highlight ? '#ff3333' : '#ffffff');// highlight is set on touch callback 
+      if (highlight) {// if highlight set a timeout to reset to normal
         setTimeout(function () {
           notifications.printNotification(notification, false);
         }, 1000);
@@ -120,12 +122,12 @@
       if (notifications.active) {
         for (var i = 0; i < notifications.list.length; i++) {
           if (notifications.list[i].y === y) {
-            notifications.printNotification(notifications.list[i], true);
-            ap37.openNotification(notifications.list[i].id);
+            notifications.printNotification(notifications.list[i], true);// highlight touched
+            ap37.openNotification(notifications.list[i].id);// and open
             return;
           }
         }
-      } else if (y === 3) {
+      } else if (y === notifstart) {// permission request alert on line  3
         ap37.requestNotificationsPermission();
       }
     }
@@ -134,7 +136,7 @@
   var apps = {
     list: [],
     lineHeight: 2,
-    topMargin: 6,
+    topMargin: appstart,
     bottomMargin: 8,
     lines: 0,
     appWidth: 6,
@@ -144,7 +146,6 @@
     isNextPageButtonVisible: false,
     printPage: function (page) {
       var appPos = page * apps.appsPerPage;
-
       for (var x = 0; x + apps.appWidth <= w; x += apps.appWidth) {
         for (var y = apps.topMargin; y < apps.topMargin + apps.lines *
         apps.lineHeight; y += apps.lineHeight) {
@@ -173,22 +174,28 @@
       }
     },
     init: function () {
-      apps.list = ap37.getApps();
+      appslist = ap37.getApps();
+      for (var i = 0; i<appslist.length; i++){
+       var app=appslist[i];
+       if (!hideapps.includes(app.name)){
+        apps.list.push(app);
+       }
+      }
       apps.lines = Math.floor(
         (h - apps.topMargin - apps.bottomMargin) / apps.lineHeight);
       apps.appsPerLine = Math.ceil(apps.list.length / apps.lines);
       apps.appWidth = Math.floor(w / apps.appsPerLine);
 
       // check minimum app name length
-      if (apps.appWidth < 6) {
-        apps.appWidth = 6;
+      if (apps.appWidth < appnameminwidth) {//if available app width is too small
+        apps.appWidth = appnameminwidth;// force
         apps.appsPerLine = Math.floor(w / apps.appWidth);
-        apps.isNextPageButtonVisible = true;
+        apps.isNextPageButtonVisible = true;// and activate pagination
         print(w - 4, h - 9, '>>>');
         print(w - 4, h - 8, '>>>');
       } else {
         apps.isNextPageButtonVisible = false;
-        background.printPattern(w - 4, w, h - 9);
+        background.printPattern(w - 4, w, h - 9);// erase pagination >>>
       }
 
       apps.appsPerPage = apps.lines * apps.appsPerLine;
@@ -196,7 +203,7 @@
 
       apps.printPage(apps.currentPage);
 
-      ap37.setOnAppsListener(apps.init);
+      ap37.setOnAppsListener(apps.init);// reset app list callback
     },
     onTouch: function (x, y) {
       for (var i = apps.currentPage * apps.appsPerPage; i <
@@ -221,188 +228,12 @@
     }
   };
 
-  var markets = {
-    update: function () {
-      get('https://api.cryptowat.ch/markets/prices', function (response) {
-        try {
-          var result = JSON.parse(response).result,
-            marketString =
-              'BTC' + Math.floor(result['market:kraken:btcusd']) +
-              ' BCH' + Math.floor(result['market:kraken:bchusd']) +
-              ' ETH' + Math.floor(result['market:kraken:ethusd']) +
-              ' ETC' + Math.floor(result['market:kraken:etcusd']) +
-              ' LTC' + Math.floor(result['market:kraken:ltcusd']) +
-              ' ZEC' + Math.floor(result['market:kraken:zecusd']);
-          background.printPattern(0, w, h - 7);
-          print(0, h - 7, marketString);
-        } catch (e) {
-        }
-      });
-    },
-    init: function () {
-      print(0, h - 8, '// Markets');
-      markets.update();
-      setInterval(markets.update, 60000);
-    }
-  };
-
-  var transmissions = {
-    list: [],
-    update: function () {
-      get('https://hacker-news.firebaseio.com/v0/topstories.json', function (response) {
-        try {
-          var result = JSON.parse(response),
-            line = h - 4,
-            t = transmissions;
-          t.list = [];
-          for (var i = 0; i < result.length && i < 3; i++) {
-            get('https://hacker-news.firebaseio.com/v0/item/' + result[i] + '.json', function (itemResponse) {
-              var itemResult = JSON.parse(itemResponse);
-              var transmission = {
-                title: itemResult.title,
-                url: itemResult.url,
-                y: line
-              };
-              t.list.push(transmission);
-              background.printPattern(0, w, line);
-              t.printTransmission(transmission, false);
-              line++;
-            });
-          }
-        } catch (e) {
-        }
-      });
-    },
-    printTransmission: function (transmission, highlight) {
-      print(0, transmission.y, transmission.title,
-        highlight ? '#ff3333' : '#ffffff');
-      if (highlight) {
-        setTimeout(function () {
-          transmissions.printTransmission(transmission, false);
-        }, 1000);
-      }
-    },
-    init: function () {
-      print(0, h - 5, '// Transmissions');
-      transmissions.update();
-      setInterval(transmissions.update, 3600000);
-    },
-    onTouch: function (x, y) {
-      for (var i = 0; i < transmissions.list.length; i++) {
-        if (transmissions.list[i].y === y &&
-          x <= transmissions.list[i].title.length) {
-          transmissions.printTransmission(transmissions.list[i], true);
-          ap37.openLink(transmissions.list[i].url);
-          return;
-        }
-      }
-    }
-  };
-
-  var wordGlitch = {
-    tick: 0,
-    length: 0,
-    x: 0,
-    y: 0,
-    text: [],
-    active: false,
-    intervalId: null,
-    update: function () {
-      var g = wordGlitch;
-      if (g.tick === 0) { // generate new glitch
-        g.length = 5 + Math.floor(Math.random() * 6);
-        g.x = Math.floor(Math.random() * (w - g.length));
-        g.y = Math.floor(Math.random() * h);
-
-        g.text = [];
-        for (var i = 0; i < 5; i++) {
-          g.text.push(Math.random().toString(36).substr(2, g.length));
-        }
-
-        ap37.print(g.x, g.y, g.text[g.tick], '#666666');
-        g.tick++;
-      } else if (g.tick === 5) { // remove glitch
-        ap37.printMultipleColors(g.x, g.y,
-          background.buffer[g.y].substr(g.x, g.length),
-          background.bufferColors[g.y].slice(g.x, g.x + g.length)
-        );
-        g.tick = 0;
-        if (!wordGlitch.active) {
-          clearInterval(wordGlitch.intervalId);
-        }
-      } else {
-        ap37.print(g.x, g.y, g.text[g.tick], '#666666');
-        g.tick++;
-      }
-    },
-    onTouch: function (x, y) {
-      if (x > w - 6 && y > h - 4) {
-        wordGlitch.active = !wordGlitch.active;
-        if (wordGlitch.active) {
-          wordGlitch.intervalId = setInterval(wordGlitch.update, 100);
-        }
-      }
-    }
-  };
-
-  var lineGlitch = {
-    tick: 0,
-    line: 0,
-    active: false,
-    intervalId: null,
-    update: function () {
-      var g = lineGlitch;
-      if (g.tick === 0) { // shift line
-        g.line = 1 + Math.floor(Math.random() * h - 1);
-
-        var offset = 1 + Math.floor(Math.random() * 4),
-          direction = Math.random() >= 0.5;
-
-        if (direction) {
-          ap37.printMultipleColors(0, g.line,
-            rightPad(
-              background.buffer[g.line].substring(offset), w,
-              ' '),
-            background.bufferColors[g.line].slice(offset));
-        } else {
-          ap37.printMultipleColors(0, g.line,
-            leftPad(background.buffer[g.line]
-              .substring(0, w - offset), w, ' '),
-            arrayFill('#ffffff', offset)
-              .concat(background.bufferColors[g.line]
-                .slice(0, w - offset))
-          );
-        }
-        g.tick++;
-      } else { // restore line
-        ap37.printMultipleColors(
-          0, g.line, background.buffer[g.line],
-          background.bufferColors[g.line]);
-        g.tick = 0;
-        if (!lineGlitch.active) {
-          clearInterval(lineGlitch.intervalId);
-        }
-      }
-    },
-    onTouch: function (x, y) {
-      if (x > w - 6 && y > h - 4) {
-        lineGlitch.active = !lineGlitch.active;
-        if (lineGlitch.active) {
-          lineGlitch.intervalId = setInterval(lineGlitch.update, 200);
-        }
-      }
-    }
-  };
-
   //utils
 
   function print(x, y, text, color) {
     color = color || '#ffffff';
     background.buffer[y] = background.buffer[y].substr(0, x) + text +
       background.buffer[y].substr(x + text.length);
-    for (var i = x; i < x + text.length; i++) {
-      background.bufferColors[y][i] = color;
-    }
     ap37.print(x, y, text, color);
   }
 
@@ -438,6 +269,6 @@
   }
 
   init();
-})();
+
 
 // pull requests github.com/apseren/ap37
