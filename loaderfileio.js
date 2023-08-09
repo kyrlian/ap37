1044cc7409c7a3a3a23ba65c8a4fb1577cf14b5c
kyrlian <63187993+kyrlian@users.noreply.github.com>
2023-08-09 12:58:27 +0200

    Update loaderfileio.js

--- loaderfileio.js
+++ loaderfileio.js
@@ -31,33 +31,33 @@ function getfilekey(filename,callback){
 function runscript(key){
   const data = null;
   const fileurl = "https://file.io/"+key
-  print("runscript fileurl:"+fileurl);
+  print("runscript fileurl:"+fileurl);  
   const xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
   xhr.open("GET", fileurl);
   xhr.setRequestHeader("Authorization", "Bearer " + fileioapikey);
-  xhr.onload = function () {
-   print("runscript status:"+xhr.status);
-   if (xhr.status === 200) {
-    print("runscript xhr:"+xhr);
-    print("runscript response:"+xhr.response);
-    eval(xhr.response);
-   }
-  };
+  xhr.addEventListener("readystatechange", function () {
+    print("runscript readyState:"+this.readyState);
+    if (this.readyState === this.DONE) {
+      let response = this.responseText;
+      printlines("runscript response:"+response;
+      eval(response);
+    }
+  });
   xhr.send(data);
 }
 
 var x = 1;
 var y = 0;
 function print(text) {
-  y ++;
+  y++;
   ap37.print(x, y, text, "#ffffff");
 }
 
 function printlines(text){
-  let pos=0
+  let pos=0;
   while(pos < text.length){    
-    print(text.substring(pos, Math.min(pos+w,text.length));
+    print(text.substring(pos, Math.min(pos+w,text.length)));
     pos += w;
   }
 }
