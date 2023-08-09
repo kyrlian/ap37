d3bf32a32b3bfb01d1e45bc4a991ec9f8ab95165
kyrlian <63187993+kyrlian@users.noreply.github.com>
2023-08-09 11:59:31 +0200

    Update loaderfileio.js

--- loaderfileio.js
+++ loaderfileio.js
@@ -1,5 +1,5 @@
 const scriptname = "custom.js";
-const bearer="Bearer 4NA6HH6.VFV2R08-1KTMWRR-M7PKQAZ-JHVEZ21";
+const fileioapikey = "4NA6HxxEZ21";
 
 function getfilekey(filename,callback){
   const data = "search="+filename;
@@ -7,10 +7,14 @@ function getfilekey(filename,callback){
   xhr.withCredentials = true;
   xhr.open("GET", "https://file.io/");
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.setRequestHeader("Authorization", bearer);
+  xhr.setRequestHeader("Authorization", "Bearer "+fileioapikey);
   xhr.onload = function () {
+   ap37.print(2, "getfilekey status:"xhr.status);
    if (xhr.status === 200) {
-    callback(JSON.parse(xhr.response).nodes[0].key)
+     ap37.print(4, "response:"+xhr.response);
+     let key = JSON.parse(xhr.response).nodes[0].key
+     ap37.print(6, "key:"key);
+     callback(key)
    }
   };
   xhr.send(data);
@@ -20,12 +24,18 @@ function runscript(key){
   const xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
   xhr.onload = function () {
+   ap37.print(8, "runscript status:"xhr.status);
    if (xhr.status === 200) {
     eval(xhr.response)
    }
   };
   xhr.open("GET", "https://file.io/"+key);
-  xhr.setRequestHeader("Authorization", bearer);
+  xhr.setRequestHeader("Authorization", "Bearer "+fileioapikey);
   xhr.send(null);
+}
+
+function print(y, text) {
+  ap37.print(0, y, text, "#ffffff#);
+}
 
 getfilekey(scriptname,runscript);
